c7138a5c68486ef17ce417ca90ce1f2a6ce96cc3
Gabriel Florit <gabrielflorit@gmail.com>
2019-03-17 13:56:26 -0400

    Update

---
+++ precache-manifest.9c5c85eae8ec9187d51ae9661b85149a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f29b3c9cd3c2fc029081",
+    "url": "https://script8.github.io/static/css/main.9d73eba9.chunk.css"
+  },
+  {
+    "revision": "f29b3c9cd3c2fc029081",
+    "url": "https://script8.github.io/static/js/main.83831d22.chunk.js"
+  },
+  {
+    "revision": "55aaba4c011df5d7d3df",
+    "url": "https://script8.github.io/static/js/runtime~main.f05a3785.js"
+  },
+  {
+    "revision": "ef28a11ad331ea8c798b",
+    "url": "https://script8.github.io/static/js/2.a1415188.chunk.js"
+  },
+  {
+    "revision": "e3552630d2013510a2427685ac569e87",
+    "url": "https://script8.github.io/static/media/VT323-latin.e3552630.woff2"
+  },
+  {
+    "revision": "30fc42eeb6df8179700ca65156e8a4ed",
+    "url": "https://script8.github.io/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "https://script8.github.io/precache-manifest.b392f08127411a10d1b8a95b815e55f1.js"
+  "https://script8.github.io/precache-manifest.9c5c85eae8ec9187d51ae9661b85149a.js"
 );
 
 workbox.clientsClaim();

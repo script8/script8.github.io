a64f16047906a1c32b9c5703f87f2d6c6056b688
Gabriel Florit <gabrielflorit@gmail.com>
2019-03-17 13:50:32 -0400

    Update

---
+++ precache-manifest.b392f08127411a10d1b8a95b815e55f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "eb292b506140b44c8556",
+    "url": "https://script8.github.io/static/css/main.9d73eba9.chunk.css"
+  },
+  {
+    "revision": "eb292b506140b44c8556",
+    "url": "https://script8.github.io/static/js/main.f206e9cd.chunk.js"
+  },
+  {
+    "revision": "55aaba4c011df5d7d3df",
+    "url": "https://script8.github.io/static/js/runtime~main.f05a3785.js"
+  },
+  {
+    "revision": "ef28a11ad331ea8c798b",
+    "url": "https://script8.github.io/static/js/2.a1415188.chunk.js"
+  },
+  {
+    "revision": "e3552630d2013510a2427685ac569e87",
+    "url": "https://script8.github.io/static/media/VT323-latin.e3552630.woff2"
+  },
+  {
+    "revision": "bb6f711691ab7d5b5ad27951860c51e2",
+    "url": "https://script8.github.io/index.html"
+  }
+];
